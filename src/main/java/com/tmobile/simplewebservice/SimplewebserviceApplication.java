4437a7e756914e98dfac5665a2639ff5a5df89e0
Felipe Ham <Felipe.G.Ham@sprint.com>
2021-05-21 16:39:03 -0500

    Initial commit

---
+++ src/main/java/com/tmobile/simplewebservice/SimplewebserviceApplication.java
@@ -0,0 +1,13 @@
+package com.tmobile.simplewebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplewebserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplewebserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tmobile/simplewebservice/SimplewebserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tmobile.simplewebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplewebserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
